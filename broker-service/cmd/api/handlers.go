32669d31eaf0084a5a41f611d26286b34a6582cd
bytemoves <morganmwitti@gmail.com>
2024-04-15 16:42:44 +0300

    microservice

---
+++ broker-service/cmd/api/handlers.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	
+	"net/http"
+)
+
+
+func ( app *Config) Broker ( w http.ResponseWriter , r *http.Request){
+	payload := jsonResponse{
+		Error: false,
+		Message: "Hit the broker",
+	}
+
+	_ = app.WriteJSON(w,http.StatusOK,payload)
+}
